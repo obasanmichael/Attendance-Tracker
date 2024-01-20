cefb7845265d586b135160bc7e1e63a93fda9077
Obasan Tolulope <obasantolu@gmail.com>
2024-01-20 17:27:14 +0100

    Installed bcrypt

--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,4 @@
-import mongoose from 'mongoose';
+const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
   fullName: { type: String, required: true },
@@ -6,6 +6,10 @@ const userSchema = new mongoose.Schema({
   level: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  enrolledCourses: [{ type: mongoose.Schema.Types.ObjectId, ref: "Course" }],
+  attendanceRecords: [
+    { type: mongoose.Schema.Types.ObjectId, ref: "Attendance" },
+  ],
 });
 
 const User = mongoose.model("User", userSchema);
