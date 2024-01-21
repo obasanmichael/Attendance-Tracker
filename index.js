91bed6ec84c760670f84389dc3cb0cab9a07c87d
Obasan Tolulope <obasantolu@gmail.com>
2024-01-21 17:26:29 +0100

    Fixed the syntax error

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
+import { fileURLToPath } from "url";
+import { dirname } from "path";
 import express from "express";
 import bodyParser from "body-parser";
-import { router as userRoutes } from "./routes/userRoutes.js"; // Import as a named export
+import { router as userRoutes } from "./routes/userRoutes.js";
 import connectToDatabase from "./config/database.js";
-import User from "./src/models/user.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
 const port = 3000;
 
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+
 app.use(bodyParser.json());
 
+connectToDatabase();
 
+app.get("/login", (req, res) => {
+  res.render("login"); 
+});
 
-connectToDatabase()
+app.get("/signup", (req, res) => {
+  res.render("signup"); 
+});
 
 app.use("/api/user", userRoutes);
 
